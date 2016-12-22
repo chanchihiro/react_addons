bcebfb58e86b2559d8a74189f159420197615ead
chanchi- <chanchi0707@gmail.com>
2016-12-23 03:18:55 +0900

    エラーが消えない

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,20 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from 'react'
+import ReactDOM from 'react-dom'
 import Perf from 'react-addons-perf'
 
 
-let tableClumns = ["名前", "地域", "番号"];
+let tableClumns = ['名前', '地域', '番号'];
 let tableData = [
-	{id: 1, name: "野口千紘", area: "東京都港区", number: "888888"},
-	{id: 2, name: "山田太郎", area: "東京都渋谷区", number: "000000"},
-	{id: 3, name: "鈴木謙介", area: "東京都新宿区", number: "234234"}
+	{id: 1, name: '野口千紘', area: '東京都港区', number: '888888'},
+	{id: 2, name: '山田太郎', area: '東京都渋谷区', number: '000000'},
+	{id: 3, name: '鈴木謙介', area: '東京都新宿区', number: '234234'}
 ];
 
 class ContactTable extends React.Component {
+	constructor(props){
+		super(props);
+	}
+
 	render(){
 		return(
 			<table> {this.props.children} </table>
@@ -23,9 +27,11 @@ class ContactTable.Header extends React.Component {
 		let tableTitles = this.props.title.map((cName, i) => {
 			return(<th key={i}> {cName} </th>);
 		});
-		
-		return(
-
-		);
 	}
 }
+
+
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('content')
+);

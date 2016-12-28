3228ef7cf2518bbf6ea5df1ede87eaa610dfd119
chanchi- <chanchi0707@gmail.com>
2016-12-28 22:02:21 +0900

    テーブルの表示

--- src/js/index.js
+++ src/js/index.js
@@ -1,37 +1,73 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Perf from 'react-addons-perf'
 
 
-let tableClumns = ['名前', '地域', '番号'];
+let tableColumns = ['名前', '地域', '番号'];
+
 let tableData = [
 	{id: 1, name: '野口千紘', area: '東京都港区', number: '888888'},
 	{id: 2, name: '山田太郎', area: '東京都渋谷区', number: '000000'},
 	{id: 3, name: '鈴木謙介', area: '東京都新宿区', number: '234234'}
 ];
 
-class ContactTable extends React.Component {
-	constructor(props){
-		super(props);
-	}
 
+class TableHeader extends React.Component {
 	render(){
+		let tableTitles = this.props.title.map((cName, i) => {
+			return(
+				<th key={i}> 
+					{cName}
+				</th>
+			);
+		});
+
 		return(
-			<table> {this.props.children} </table>
+			<thead>
+				<tr>
+					{tableTitles}
+				</tr>
+			</thead>
 		);
 	}
 }
 
-class ContactTable.Header extends React.Component {
+class TableBody extends React.Component {
 	render(){
-		let tableTitles = this.props.title.map((cName, i) => {
-			return(<th key={i}> {cName} </th>);
+		let tableRows = this.props.data.map((person) => {
+			return(
+				<tr key={person.id}>
+					<td>{person.name}</td>
+					<td>{person.area}</td>
+					<td>{person.number}</td>
+				</tr>
+			);
 		});
+
+		return(
+			<tbody>
+				{tableRows}
+			</tbody>
+		);
+	}
+}
+
+class DispTable extends React.Component {
+	render(){
+		return(
+			<table className="bd">
+				<TableHeader title={this.props.title} />
+				<TableBody data={this.props.data} />
+			</table>
+		);
 	}
 }
 
 
 ReactDOM.render(
-  <CommentBox />,
-  document.getElementById('content')
+	<DispTable title={tableColumns} data={tableData} />,
+	document.getElementById("content")
 );
+
+
+
+
